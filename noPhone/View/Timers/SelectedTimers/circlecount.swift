af4175dc8b678c168d8f22cfef758f8e3572dbc7
dogofsunrising <tengbenkouta@gmail.com>
2025-01-15 15:44:28 +0900

    circleタイマーを作成

--- noPhone/View/Timers/SelectedTimers/circlecount.swift
+++ noPhone/View/Timers/SelectedTimers/circlecount.swift
@@ -8,6 +8,10 @@ struct circleTimer: Count {
     var seconds:Int
     var CGTimer:CGFloat = 1
     var up:Bool = false
+    let colors: [Color] = [.blue, .red, .green, .purple, .orange]
+    
+    var precurrntposition:Int = 0
+    var currntposition:Int = 0
     init(timer: Int) {
         self.timer = timer
         self.hours = timer / 3600
@@ -20,7 +24,12 @@ struct circleTimer: Count {
         // settimeが0の場合は3600に設定
         self.settime = (settime == 0) ? 3600 : settime
         self.timer = timer
-        self.CGTimer = CGFloat(self.timer-1) / CGFloat(self.settime)
+        self.up = up
+        if up {
+            self.CGTimer = CGFloat(self.timer+1) / CGFloat(self.settime)
+        } else {
+            self.CGTimer = CGFloat(self.timer-1) / CGFloat(self.settime)
+        }
     }
 
     var body: some View{
@@ -32,15 +41,27 @@ struct circleTimer: Count {
                 .foregroundColor(.gray)
                 .rotationEffect(Angle(degrees: 270.0))
             if(up){
-                
+                ForEach(0..<100) { num in
+                    if(timer/3600 >= num) {
+                        Circle()
+                            .trim(from: 0, to: CGTimer - CGFloat(num))
+                            .stroke(lineWidth: 20.0)
+                            .frame(width: 250, height: 250)
+                            .foregroundColor(colors[num%6])
+                            .rotationEffect(Angle(degrees: 270.0))
+                            .animation(.easeInOut(duration: 1.5), value: CGTimer - CGFloat(num))
+                    }
+                }
+            } else {
+                Circle()
+                    .trim(from: 0, to: CGTimer)
+                    .stroke(lineWidth: 20.0)
+                    .frame(width: 250, height: 250)
+                    .foregroundColor(colors[currntposition])
+                    .rotationEffect(Angle(degrees: 270.0))
+                    .animation(.easeInOut(duration: 1.5), value: CGTimer)
             }
-            Circle()
-                .trim(from: 0, to: CGTimer)
-                .stroke(lineWidth: 20.0)
-                .frame(width: 250, height: 250)
-                .foregroundColor(.blue)
-                .rotationEffect(Angle(degrees: 270.0))
-                .animation(.easeInOut(duration: 1.5), value: CGTimer)
+            
             Text(String(format: "%02dh%02dm%02ds", hours, minutes, seconds))
                 .font(.title2)
                 .fontWeight(.bold)
